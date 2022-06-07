94ca052435a0474ce10d4ca91958f385f41e84db
Firna Firdiani <70745005+vyrnvyrd@users.noreply.github.com>
2022-06-07 20:38:55 +0700

    feat(dosen): add function to insert new dosen

--- mll.cpp
+++ mll.cpp
@@ -1,2 +1,25 @@
 #include <iostream>
 #include "mll.h"
+
+void Create_List(mll &list_dosen){
+    first(list_dosen) = nil;
+}
+
+void New_Elm_Dosen(dosen dataDosen, adr_dosen &adrDosen) {
+    adrDosen = new elm_dosen;
+    nextDosen(adrDosen) = nil;
+    nextMahasiswa(adrDosen) = nil;
+    info(adrDosen) = dataDosen;
+}
+
+void Insert_Last_Dosen(mll &list_dosen, adr_dosen adrDosen) {
+    if (first(list_dosen) == nil) {
+        first(list_dosen) = adrDosen;
+    } else {
+        adr_dosen P = first(list_dosen);
+        while (nextDosen(P) != nil) {
+            P = nextDosen(P);
+        }
+        nextDosen(P) = adrDosen;
+    }
+}
--- mll.h
+++ mll.h
@@ -33,4 +33,8 @@ struct mll {
     adr_dosen first;
 };
 
+void Create_List(mll &list_dosen);
+void New_Elm_Dosen(dosen dataDosen, adr_dosen &adrDosen);
+void Insert_Last_Dosen(mll &list_dosen, adr_dosen adrDosen);
+
 #endif // MLL_H_INCLUDED
