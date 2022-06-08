f53ee6eaf2430ab867add45195dd16b536e042f6
Firna Firdiani <70745005+vyrnvyrd@users.noreply.github.com>
2022-06-08 20:40:48 +0700

    feat(dosen): delete dosen by dosen code

--- main.cpp
+++ main.cpp
@@ -8,15 +8,24 @@ int main()
     // temp logic,
     mll list_dosen;
     Create_List(list_dosen);
-    adr_dosen adrDosen;
+    adr_dosen adrDosen, dataDelete;
 
     //make sure procedure Insert_Last_Dosen
     New_Elm_Dosen({"11111", "Ida", "ID", "Dasar-Dasar Pemograman"}, adrDosen);
     Insert_Last_Dosen(list_dosen, adrDosen);
     New_Elm_Dosen({"22222", "Ardi", "ARD", "Algoritma Pemrograman"}, adrDosen);
     Insert_Last_Dosen(list_dosen, adrDosen);
+    New_Elm_Dosen({"33333", "Bima", "BM", "Kalkulus"}, adrDosen);
+    Insert_Last_Dosen(list_dosen, adrDosen);
+    New_Elm_Dosen({"44444", "Cintya", "CN", "Bhs Inggris"}, adrDosen);
+    Insert_Last_Dosen(list_dosen, adrDosen);
 
-    showDosenData(list_dosen,"ARD");
+//    Show_All_Dosen(list_dosen);
+//    dataDelete = Delete_DosenX(list_dosen, "BM");
+//    Show_All_Dosen(list_dosen);
+//    cout << "data yang didelete: " << info(dataDelete).nama << endl;
+
+//    showDosenData(list_dosen,"ARD");
 
 
     return 0;
--- mll.cpp
+++ mll.cpp
@@ -38,8 +38,8 @@ adr_dosen searchDosenByCode(mll list_dosen, string kode_dosen) {
         while(curr != nil && info(curr).kode_dosen != kode_dosen){
             curr = nextDosen(curr);
         }
-        return curr;
     }
+    return curr;
 }
 
 void showDosenData(mll list_dosen, string kode_dosen) {
@@ -52,4 +52,71 @@ void showDosenData(mll list_dosen, string kode_dosen) {
         cout << "Mata Kuliah Pengampu   : " << info(searched).matkul << endl;
         cout << "Kode Dosen             : " << info(searched).kode_dosen << endl;
     }
+}
+
+adr_dosen Delete_First_Dosen(mll &list_dosen){
+    adr_dosen P, data;
+
+    P = first(list_dosen);
+    data = first(list_dosen);
+    first(list_dosen) = nextDosen(P);
+    nextDosen(P) = nil;
+
+    return data;
+}
+
+adr_dosen Delete_Last_Dosen(mll &list_dosen){
+    adr_dosen P, Q;
+
+    P = first(list_dosen);
+    while (nextDosen(nextDosen(P)) != nil) {
+        P = nextDosen(P);
+    }
+    Q = nextDosen(P);
+    nextDosen(P) = nil;
+
+    return Q;
+}
+
+adr_dosen Delete_After_Dosen(mll &list_dosen, adr_dosen prev, adr_dosen curr){
+    curr = nextDosen(prev);
+    nextDosen(prev) = nextDosen(curr);
+    nextDosen(curr) = nil;
+
+    return curr;
+};
+
+adr_dosen Delete_DosenX(mll &list_dosen, string kode_dosen){
+    adr_dosen curr, prev, data;
+
+    prev = nil;
+    data = nil;
+    curr = first(list_dosen);
+    while(curr != nil && info(curr).kode_dosen != kode_dosen){
+        prev = curr;
+        curr = nextDosen(curr);
+    }
+
+    if (curr != nil){
+        if (prev == nil && curr == first(list_dosen)) {
+            data = Delete_First_Dosen(list_dosen);
+        } else if (nextDosen(curr) == nil) {
+            data = Delete_Last_Dosen(list_dosen);
+        } else {
+            data = Delete_After_Dosen(list_dosen, prev, curr);
+        }
+    } else {
+        cout << "Data Dosen tidak ditemukan!" << endl;
+    }
+
+    return data;
+}
+
+void Show_All_Dosen(mll list_dosen){
+    adr_dosen p = first(list_dosen);
+    while(p != nil){
+        cout << info(p).nik << " - " << info(p).kode_dosen << " - " << info(p).nama << " - " << info(p).matkul << endl;
+        p = nextDosen(p);
+    }
+    cout << endl;
 }
