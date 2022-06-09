6d29961a65832636620f57dc6c5930932312ef32
Firna Firdiani <70745005+vyrnvyrd@users.noreply.github.com>
2022-06-09 22:41:44 +0700

    refactor(dosen): change logic from by code to by adr

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main()
     // temp logic add dosen
     mll list_dosen;
     Create_List(list_dosen);
-    adr_dosen adrDosen;
+    adr_dosen adrDosen, found, dataDelete;
     New_Elm_Dosen({"11111", "Ida", "ID", "Dasar-Dasar Pemograman"}, adrDosen);
     Insert_Last_Dosen(list_dosen, adrDosen);
     New_Elm_Dosen({"22222", "Ardi", "ARD", "Algoritma Pemrograman"}, adrDosen);
@@ -25,13 +25,17 @@ int main()
     New_Elm_Mahasiswa({"23456", "Mazid", "Pengembangan TI"}, adrMahasiswa);
     Insert_New_Mahasiswa(list_dosen, searchDosenByCode(list_dosen, "ID"), adrMahasiswa);
 
-//    Show_All_Dosen(list_dosen);
-//    dataDelete = Delete_DosenX(list_dosen, "BM");
-//    Show_All_Dosen(list_dosen);
-//    cout << "data yang didelete: " << info(dataDelete).nama << endl;
-
-//    showDosenData(list_dosen,"ARD");
-
-
+    cout << "----------------------------------------------------------" << endl;
+    showDosenData(list_dosen,"ARD");
+    cout << "----------------------------------------------------------" << endl;
+    Show_All_Dosen(list_dosen);
+    found = searchDosenByCode(list_dosen, "ARD");
+    cout << "ketemu: " << info(found).nama << endl;
+    if (found != nil) {
+        dataDelete = Delete_DosenX(list_dosen, found);
+    }
+    Show_All_Dosen(list_dosen);
+    cout << "data yang didelete: " << info(dataDelete).nama << endl;
+    cout << "---------------------------------------------------------" << endl;
     return 0;
 }
